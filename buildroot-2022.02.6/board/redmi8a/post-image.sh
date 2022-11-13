21d91546459cb4d9b4e37cc99c37238502552606
luhux76@gmail.com <luhux76@gmail.com>
2022-11-13 20:49:48 +0800

    2022 11 13

---
+++ buildroot-2022.02.6/board/redmi8a/post-image.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+
+BOARD_DIR="$(dirname $0)"
+
+abootimg --create ${BINARIES_DIR}/redmi8a.img \
+	-f ${BOARD_DIR}/bootimg.cfg \
+	-k ${BINARIES_DIR}/zImage-dtb \
+	-r ${BINARIES_DIR}/rootfs.cpio.gz
