e0af3fcfcee47fc6cdb6da511d98861c1b581d43
luhux76@gmail.com <luhux76@gmail.com>
2022-11-14 20:12:37 +0800

    2022/11/14

--- board/redmi8a/post-image.sh
+++ board/redmi8a/post-image.sh
@@ -6,5 +6,5 @@ BOARD_DIR="$(dirname $0)"
 
 abootimg --create ${BINARIES_DIR}/redmi8a.img \
 	-f ${BOARD_DIR}/bootimg.cfg \
-	-k ${BINARIES_DIR}/zImage-dtb \
+	-k ${BINARIES_DIR}/Image.gz-dtb \
 	-r ${BINARIES_DIR}/rootfs.cpio.gz
