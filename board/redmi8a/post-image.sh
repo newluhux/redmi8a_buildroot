28c9fc5facad8e09a1b536d2d16d2a5fea23e838
luhux76@gmail.com <luhux76@gmail.com>
2022-11-14 23:46:12 +0800

    configs/redmi8a_defconfig: switch 32bit kernel build

--- board/redmi8a/post-image.sh
+++ board/redmi8a/post-image.sh
@@ -6,5 +6,11 @@ BOARD_DIR="$(dirname $0)"
 
 abootimg --create ${BINARIES_DIR}/redmi8a.img \
 	-f ${BOARD_DIR}/bootimg.cfg \
-	-k ${BINARIES_DIR}/Image.gz-dtb \
+	-k ${BINARIES_DIR}/zImage-dtb \
 	-r ${BINARIES_DIR}/rootfs.cpio.gz
+
+# drity, need rewrite
+mkdtboimg create ${BINARIES_DIR}/dtbo.img \
+	${BUILD_DIR}/linux-custom/arch/arm/boot/dts/*/*.dtbo
+
+exit $?
